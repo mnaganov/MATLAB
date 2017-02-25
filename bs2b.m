a4ef1125a2a6839ae21df012a8b4861333785e4d
Mikhail Naganov <mikhail.naganov@gmail.com>
2017-02-25 10:11:21 -0800

    Initial commit

---
+++ bs2b.m
@@ -0,0 +1,9 @@
+plot_filter(
+  20,
+  20000, 
+  '/Users/mikhail/Sounds/MeasSweep_44100_20_20000_L-Mono.wav',
+  '/Users/mikhail/Sounds/bs2b-MeasSweep_44100_20_20000_L-Left.wav',
+  '/Users/mikhail/Sounds/bs2b-MeasSweep_44100_20_20000_L-Right.wav',
+  [-100, 300],
+  200);
+  
