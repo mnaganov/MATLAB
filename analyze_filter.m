205ac6182b930b963cfd6e37c15ae7027c8a110f
Mikhail Naganov <mikhail.naganov@gmail.com>
2017-06-17 16:14:56 +0200

    Units correction

--- analyze_filter.m
+++ analyze_filter.m
@@ -1,4 +1,4 @@
-function [freqs, l_freq_resp, l_phase_resp, l_gd, r_freq_resp, r_phase_resp, r_gd] = analyze_filter (start_fq, end_fq, stim_file, resp_file, gd_smoothing)
+function [freqs, l_freq_resp, l_phase_resp, l_gd_us, r_freq_resp, r_phase_resp, r_gd_us] = analyze_filter (start_fq, end_fq, stim_file, resp_file, gd_smoothing)
   [stim_wave_lr, s_rate] = audioread(stim_file);
   [resp_wave_lr, resp_sr] = audioread(resp_file);
 
@@ -40,11 +40,11 @@ function [freqs, l_freq_resp, l_phase_resp, l_gd, r_freq_resp, r_phase_resp, r_g
   freqs = all_freqs(start_pos:end_pos);
 
   fft_stim_wave = fft(stim_wave);
-  [l_freq_resp, l_phase_resp, l_gd] = analyze_channel(start_pos, end_pos, fft_bin, fft_stim_wave, l_resp_wave, gd_smoothing);
-  [r_freq_resp, r_phase_resp, r_gd] = analyze_channel(start_pos, end_pos, fft_bin, fft_stim_wave, r_resp_wave, gd_smoothing);
+  [l_freq_resp, l_phase_resp, l_gd_us] = analyze_channel(start_pos, end_pos, fft_bin, fft_stim_wave, l_resp_wave, gd_smoothing);
+  [r_freq_resp, r_phase_resp, r_gd_us] = analyze_channel(start_pos, end_pos, fft_bin, fft_stim_wave, r_resp_wave, gd_smoothing);
 endfunction
 
-function [freq_resp, phase_resp, gd] = analyze_channel (start_pos, end_pos, fft_bin, fft_stim_wave, resp_wave, gd_smoothing)
+function [freq_resp, phase_resp, gd_us] = analyze_channel (start_pos, end_pos, fft_bin, fft_stim_wave, resp_wave, gd_smoothing)
   fft_resp_wave = fft(resp_wave);
   fft_filter = fft_resp_wave ./ fft_stim_wave;
   freq_resp = abs(fft_filter(start_pos:end_pos));
@@ -56,5 +56,5 @@ function [freq_resp, phase_resp, gd] = analyze_channel (start_pos, end_pos, fft_
   end;
 
   gd = -diff(sm_phase_resp) / (fft_bin * 2 * pi);
-  gd = gd(start_pos:end_pos - 1) * 1000000;
+  gd_us = gd(start_pos:end_pos - 1) * 1000000;
 endfunction
