6281bc1c05c5f7c62adcea428aa4db540cb83518
Mikhail Naganov <mikhail.naganov@gmail.com>
2017-08-27 19:43:46 -0700

    Modelling ideal IR of Phonitor

--- plot_phonitor_measured_vs_modelled.m
+++ plot_phonitor_measured_vs_modelled.m
@@ -1,9 +1,9 @@
-l_file = sound_data_dir('Phonitor-Med-30-1.2-FR-Left-1_3.csv');
-r_file = sound_data_dir('Phonitor-Med-30-1.2-FR-Right-1_3.csv');
+l_file = sound_data_dir('Phonitor-Med-30-1.2-L-96.csv');
+r_file = sound_data_dir('Phonitor-Med-30-1.2-R-96.csv');
 
 [meas_frqs, _, meas_fq_resp] = filter_from_two_log_csv_files(l_file, r_file);
 
-[mdl_tf, n_fft, fs] = tf_from_two_log_csv_files(l_file, r_file, 2048, 44100, 8, 8);
+[mdl_tf, n_fft, fs] = tf_from_two_log_csv_files(l_file, r_file, 8192, 96000, 12, 12);
 [mdl_frqs, mdl_fq_lim, mdl_fq_resp] = filter_from_tf([20, 20000], mdl_tf, n_fft, fs);
 
 plot_filter_vs(
--- process_by_phonitor.m
+++ process_by_phonitor.m
@@ -1,5 +1,10 @@
-[tf, n_fft, fs] = experimental_phonitor_tf();
+l_file = sound_data_dir('Phonitor-Med-30-1.2-L-96.csv');
+r_file = sound_data_dir('Phonitor-Med-30-1.2-R-96.csv');
+
+[meas_frqs, _, meas_fq_resp] = filter_from_two_log_csv_files(l_file, r_file);
+[tf, n_fft, fs] = tf_from_two_log_csv_files(l_file, r_file, 8192, 96000, 12, 12);
+
 apply_binaural_filter(
-    sound_data_dir('fzm-stimulus-leftch.wav'),
+    sound_data_dir('impulse-96-LeftCh.wav'),
     tf, fs,
-    sound_data_dir('fzm-phonitor-response.wav'));
+    sound_data_dir('phonitor-96-IR.wav'));
