a3b33b09c909b8763555b4d27d1a33cc75a2c820
Mikhail Naganov <mikhail.naganov@gmail.com>
2017-11-08 21:47:42 -0800

    Re-created Morphit for SRH1540

--- apply_filter_mono.m
+++ apply_filter_mono.m
@@ -1,11 +1,24 @@
 function apply_filter_mono (stim_file, tf, fs, resp_file)
   [stim_wave, s_rate] = audioread(stim_file);
-  if (columns(stim_wave) ~= 1)
+  [~, channels] = size(stim_wave);
+  if (channels ~= 1)
     error("stimulus file must be mono");
   end
   if (s_rate ~= fs)
     error("stimulus file sampling rate %d ~= filter sampling rate %d", s_rate, fs);
   end
+  stim_info = audioinfo(stim_file);
   resp_wave = filter(tf.B, tf.A, stim_wave);
-  audiowrite(resp_file, resp_wave, fs);
+  audiowrite(resp_file, resp_wave, fs, ...
+             'BitsPerSample', stim_info.BitsPerSample, ...
+             'Title', optional_param(stim_info.Title), ...
+             'Artist', optional_param(stim_info.Artist), ...
+             'Comment', 'Processed with IIR filter');
+end
+
+function o_p = optional_param(i_p)
+  o_p = i_p;
+  if isempty(o_p)
+    o_p = '';
+  end
 end
